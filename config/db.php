b852de494b4a5d014c174cab936171cf48330c71
TorikulIslamPatoary <patoarytorikulislam@gmail.com>
2026-01-21 21:25:18 -0800

    Update db.php

--- config/db.php
+++ config/db.php
@@ -1,5 +1,5 @@
 <?php
-$conn = mysqli_connect("localhost", "root", "", "university_db");
+$conn = mysqli_connect("localhost", "root", "", "university_db"3307);
 
 if (!$conn) {
     die("Database connection failed");
